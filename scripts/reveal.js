343ac0e04a131415d9679e50d2ff32cf8e496e37
ben-sack <mr.benjaminsack@gmail.com>
2022-04-27 17:00:32 -0700

    first commit

---
+++ scripts/reveal.js
@@ -0,0 +1,35 @@
+function reveal() {
+  var x = document.getElementById("reveal");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function reveal2() {
+  var x = document.getElementById("reveal2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function reveal3() {
+  var x = document.getElementById("reveal3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function reveal4() {
+  var x = document.getElementById("reveal4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
