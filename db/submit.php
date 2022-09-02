3ccb0c85411c1acdb164396310b2df1fbdbe7703
ben-sack <mr.benjaminsack@gmail.com>
2022-09-02 16:49:18 -0700

    fix php

--- db/submit.php
+++ db/submit.php
@@ -13,21 +13,8 @@ $message = $_POST["message"];
 
 $db->exec("INSERT INTO contact_info (name, email, message) VALUES ('$name', '$email', '$message');");
 
-header("Location: /thankyou/");
+header("Location: /portfolio/thankyou/");
 
-// now output the data to a simple html table...
-//  print "<table border=1>";
-//  print "<tr><td>id</td><td>name</td><td>email</td><td>message</td>";
-
-//  $result = $db->query('SELECT * FROM contact_info');
-// foreach($result as $row)
-// {
-//   print "<tr><td>".$row['id']."</td>";
-//   print "<td>".$row['name']."</td>";
-//   print "<td>".$row['email']."</td>";
-//   print "<td>".$row['message']."</td>";
-// }
-// print "</table>";
 $db = NULL;
 }
 catch(PDOException $e)
